f6d4c701e9671bfe2bd1a37e19eff850e92cc828
ZaxarShashkov <zekharjah@gmail.com>
2023-03-09 19:05:35 +0300

    charInfo add

--- marvel_starter/src/components/charList/CharList.js
+++ marvel_starter/src/components/charList/CharList.js
@@ -1,5 +1,4 @@
 import "./charList.scss";
-import abyss from "../../resources/img/abyss.jpg";
 import { Component } from "react";
 
 import MarvelService from "../../services/MarvelService";
@@ -44,7 +43,8 @@ class CharList extends Component {
         }
         return (
             <li className="char__item"
-            key={item.id}>
+            key={item.id}
+            onClick={() => this.props.onCharSelected(item.id)}>
                 <img src={item.thumbnail} alt={item.name} style={imgStyle} />
                 <div className="char__name">{item.name}</div>
             </li>
