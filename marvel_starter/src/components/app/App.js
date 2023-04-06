b07324553e289597e35c6bc46306f5487a1cdb14
ZaxarShashkov <zekharjah@gmail.com>
2023-04-06 14:52:31 +0300

    update router v6+

--- marvel_starter/src/components/app/App.js
+++ marvel_starter/src/components/app/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
 import MainPage from "../pages/MainPage";
@@ -11,14 +11,10 @@ const App = () => {
       <div className="app">
         <AppHeader />
         <main>
-          <Switch>
-            <Route exact path="/Marvel-info">
-              <MainPage />
-            </Route>
-            <Route exact path="/Marvel-info/comics">
-              <ComicsPage />
-            </Route>
-          </Switch>
+          <Routes>
+            <Route path="/Marvel-info" element={<MainPage />} />
+            <Route path="/Marvel-info/comics" element={<ComicsPage />} />
+          </Routes>
         </main>
       </div>
     </Router>
