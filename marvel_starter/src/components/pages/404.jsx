4fd4fe6053d16a599fa1804bb21f82d86d6371f0
ZaxarShashkov <zekharjah@gmail.com>
2023-04-06 21:10:24 +0300

    page 404 fixed

--- marvel_starter/src/components/pages/404.jsx
+++ marvel_starter/src/components/pages/404.jsx
@@ -1,19 +1,23 @@
-import React, { Link } from "react";
-import ErrorMessage from "../errorMessage/ErrorMessage";
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Page404 = () => {
   return (
     <div>
-      <ErrorMessage />
       <p style={{ textAlign: "center", fontWeight: "bold", fontSize: "1.5em" }}>
         Page not Found
       </p>
-      {/* <Routes>
-        <Route style={{ display: "block", textAlign: "center" }}>
+      <p style={{ textAlign: "center", fontWeight: "bold", fontSize: "1.5em" }}>
+        <NavLink
+          end
+          style={({ isActive }) => ({
+            color: isActive ? "#9f0013" : "inherit",
+          })}
+          to="/Marvel-info"
+        >
           Back to main page
-        </Route>
-      </Routes> */}
+        </NavLink>
+      </p>
     </div>
   );
 };
